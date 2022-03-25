07af2ec72054513ababcb9c10708d165fc2ce3e5
TrentConley <trentconley@gmail.com>
2022-03-24 21:38:05 -0500

    deploying contract

--- deploy.js
+++ deploy.js
@@ -7,4 +7,8 @@ const provider = new HDWalletProvider(
   "https://rinkeby.infura.io/v3/78f48b1d5203439caaeb5c9cd4156ab1"
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+};
 // deploy code will go here
