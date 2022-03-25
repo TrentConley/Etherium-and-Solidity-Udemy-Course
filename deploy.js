1ba8c1902015824572a2a978b9152b48d9cd34ba
TrentConley <trentconley@gmail.com>
2022-03-24 21:36:46 -0500

    deploying contract

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require(",/compile");
+
+const provider = new HDWalletProvider(
+  "cat olive collect control argue soup blur equip cat luxury clump enemy",
+  "https://rinkeby.infura.io/v3/78f48b1d5203439caaeb5c9cd4156ab1"
+);
+const web3 = new Web3(provider);
 // deploy code will go here
